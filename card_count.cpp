b7bdf93aa08b32b20877e8d52a1b9295a72d5ba0
MyCabbages11011 <rowanmattson@gmail.com>
2025-02-07 12:23:28 -0500

    changed deck to a vector of struct Card

--- card_count.cpp
+++ card_count.cpp
@@ -6,34 +6,52 @@
 #include <random>
 using namespace std;
 
-vector<string> cards;
-vector<vector<string>> deck;
+struct Card {
+    string value;
+    string suit;
+};
 
+vector<Card> deck(4);
 
 int main(void)
 {
-    cards = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
-    deck = {cards, cards, cards, cards};  
-
+    vector<Card> deck = init_deck();
+    
     return 0;
 }
 
-vector<string> deal(vector<vector<string>> deck)
+vector<Card> init_deck() 
+{ 
+    //vector<Card> deck(4);
+    vector<string> suits = {"spade", "diamond", "club", "ace"};
+    for (string suit : suits) {
+        for (string value : {"2","3","4","5","6","7","8","9","10","J","Q","K","A"}) {
+            deck.push_back(Card{value,suit});
+        }
+    }
+
+    return deck;
+}
+
+vector<string> deal(vector<Card> deck)
 { 
     int selectedCard = rand() % 13 + 1;
     int selectedSuit = rand() % 4 + 1;
-    vector<string> dealing;
+    vector<Card> dealing;
 
     for(int suit = 0; suit <= 4; suit++) {
         if (suit != selectedSuit) {
             return;
         }
-        for (const auto& card : cards) {
+        for (const auto& card : deck[suit]) {
             if (stoi(card) != selectedCard)
                 {
                     return;
                 }
             dealing.push_back(card);
+            //vector<string> for_removal =  deck[suit];
+           // auto kill = remove(for_removal.begin(), for_removal.end(), card);
+           // for_removal.erase(kill, for_removal.end())
         } 
     }
     selectedCard = rand() % 13 + 1;
