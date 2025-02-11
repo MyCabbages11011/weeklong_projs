fa9c53872f8fe64c330d5097f8875a682b179c56
MyCabbages11011 <rowanmattson@gmail.com>
2025-02-11 18:38:27 -0500

    messing w/ my deck

--- card_count.cpp
+++ card_count.cpp
@@ -10,52 +10,51 @@ struct Card {
     string value;
     string suit;
 };
-
-vector<Card> deck(4);
+vector<string> suits = {"spade", "diamond", "club", "heart"};
+vector<Card> init_deck();
+vector<Card> deal(vector<Card> deck);
 
 int main(void)
 {
     vector<Card> deck = init_deck();
+    deal(deck);
     
     return 0;
 }
 
 vector<Card> init_deck() 
 { 
-    //vector<Card> deck(4);
-    vector<string> suits = {"spade", "diamond", "club", "ace"};
+    vector<Card> deck;
+    //vector<string> suits = {"spade", "diamond", "club", "heart"};
     for (string suit : suits) {
-        for (string value : {"2","3","4","5","6","7","8","9","10","J","Q","K","A"}) {
+        for (string value : {"2","3","4","5","6","7","8","9","10","J","Q","K","A"}) { //ISSUE HERE W/ FACE CARDS: stoi usage
             deck.push_back(Card{value,suit});
         }
     }
-
     return deck;
 }
 
-vector<string> deal(vector<Card> deck)
+vector<Card> deal(vector<Card> deck)
 { 
     int selectedCard = rand() % 13 + 1;
     int selectedSuit = rand() % 4 + 1;
     vector<Card> dealing;
 
-    for(int suit = 0; suit <= 4; suit++) {
-        if (suit != selectedSuit) {
-            return;
-        }
-        for (const auto& card : deck[suit]) {
-            if (stoi(card) != selectedCard)
-                {
-                    return;
-                }
-            dealing.push_back(card);
-            //vector<string> for_removal =  deck[suit];
-           // auto kill = remove(for_removal.begin(), for_removal.end(), card);
-           // for_removal.erase(kill, for_removal.end())
-        } 
+    for (const Card& card : deck) {
+        //cout << card.value << " of " << card.suit << endl;
+        if (stoi(card.value) != selectedCard)
+            {
+                continue;
+            }
+        dealing.push_back(card);
+        
+        
+
     }
+
     selectedCard = rand() % 13 + 1;
     selectedSuit = rand() % 4 + 1;
+    
 
     return dealing;
 }
@@ -65,3 +64,4 @@ int count(int card)
 
     return card;
 }
+
